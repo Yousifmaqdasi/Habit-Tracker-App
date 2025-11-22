53288c2f1a80c18e0f362205f01fe5cd05666c5d
Yousif Maqdasi <yousifmaqdasi@icloud.com>
2025-11-22 23:27:45 +0100

    Add recommended habits section and enhance habit buttons functionality

--- main.js
+++ main.js
@@ -3,10 +3,30 @@ const homePage = document.querySelector(".home-page");
 const returnHomePageBtn = document.querySelector(".home-button");
 const addHabitPage = document.querySelector(".add-habit-page");
 const addHabitButton = document.querySelector(".add-habit");
+const buildHabitBtn = document.querySelector(".build-habit");
+const quitHabitBtn = document.querySelector(".quit-habit");
+const recommendedHabitsToBuild = document.querySelector(".recommended-habits-build");
+const recommendedHabitsToQuit = document.querySelector(".recommended-habits-quit");
 
 
 
 
+
+buildHabitBtn.addEventListener("click", () => {
+    buildHabitBtn.style.backgroundColor = "#3280dabd";
+    buildHabitBtn.style.color = "white";
+    quitHabitBtn.style.backgroundColor = "";
+    recommendedHabitsToBuild.style.display = "flex";
+    recommendedHabitsToQuit.style.display = "none"
+})
+
+quitHabitBtn.addEventListener("click", () => {
+    quitHabitBtn.style.backgroundColor = "#e14631bd";
+    quitHabitBtn.style.color = "white";
+    buildHabitBtn.style.backgroundColor = "";
+    recommendedHabitsToBuild.style.display = "none";
+    recommendedHabitsToQuit.style.display = "flex"
+})
 
 
 
